8df538c2a5883005dda7a71074b72d34c438246a
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-06-12 11:26:04 +0530

    chnage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
+// ---------------------this
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+// ---------------------this
 module.exports = {
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
