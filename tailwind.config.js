8ca9947231c07a80d64d12bc84aa01f46372b513
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-06-12 13:21:41 +0530

    development

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-// ---------------------this
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-// ---------------------this
 module.exports = {
-  plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
 };
