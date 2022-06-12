e8deb91bf26f9811385ca785402a1e7ca28194bb
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-06-12 15:37:18 +0530

    chnages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
